acd80043d77e1d75d5365957d468619f1d06f966
Dan Bravender <dan.bravender@gmail.com>
2009-06-24 17:59:37 +0900

    Added a test for dicts to make sure that unique keys are generated and renamed everything, so a_dict -> dicts, a_list -> lists, etc.

--- qc.py
+++ qc.py
@@ -3,33 +3,36 @@ import types
 import os
 from functools import partial
 
-__all__ = ['an_integer', 'a_list', 'a_unicode', 'a_character', 'forall']
+__all__ = ['integers', 'lists', 'unicodes', 'characters', 'forall']
 
 def evaluate(lazy_value):
     while hasattr(lazy_value, '__call__'):
         lazy_value = lazy_value()
     return lazy_value
 
-def an_integer(low=0, high=100):
+def integers(low=0, high=100):
     return lambda: random.randint(low, high)
 
-def a_list(item_generator=an_integer, size=(0, 100)):
-    return lambda: [evaluate(item_generator) \
+def lists(items=integers, size=(0, 100)):
+    return lambda: [evaluate(items) \
                     for _ in xrange(random.randint(size[0], size[1]))]
 
-def a_dict(item_generator=an_integer, value_generator=an_integer, size=(0, 100)):
+def dicts(items=integers, values=integers, size=(0, 100)):
     def fun():
         x = {}
         for _ in xrange(random.randint(size[0], size[1])):
-            x.update({evaluate(item_generator): evaluate(value_generator)})
+            item = evaluate(items)
+            while item in x:
+                item = evaluate(items)
+            x.update({evaluate(items): evaluate(values)})
         return x
     return fun
 
-def a_unicode(size=(0, 100), minunicode=0, maxunicode=255):
+def unicodes(size=(0, 100), minunicode=0, maxunicode=255):
     return lambda: u''.join(unichr(random.randint(minunicode, maxunicode)) \
                             for _ in xrange(random.randint(size[0], size[1])))
 
-a_character = partial(a_unicode, size=(1, 1))
+characters = partial(unicodes, size=(1, 1))
 
 def forall(tries=100, **kwargs):
     def wrap(f):
@@ -46,55 +49,62 @@ def forall(tries=100, **kwargs):
     return wrap
 forall.verbose = False # if enabled will print out the random test cases
 
-@forall(tries=10, i=an_integer)
-def test_an_integer(i):
+@forall(tries=10, i=integers)
+def test_integers(i):
     assert type(i) == int
     assert i >= 0 and i <= 100
 
-@forall(tries=10, l=a_list(item_generator=an_integer))
+@forall(tries=10, l=lists(items=integers))
 def test_a_int_list(l):
     assert type(l) == list
 
-@forall(tries=10, ul=a_list(item_generator=a_unicode))
-def test_a_unicode_list(ul):
+@forall(tries=10, ul=lists(items=unicodes))
+def test_unicodes_list(ul):
     assert type(ul) == list
+    if len(ul):
+        assert type(ul[0]) == unicode
 
-@forall(tries=10, l=a_list(item_generator=an_integer, size=(10, 50)))
-def test_a_list_size(l):
+@forall(tries=10, l=lists(items=integers, size=(10, 50)))
+def test_lists_size(l):
     assert len(l) <= 50 and len(l) >= 10
 
-@forall(tries=10, u=a_unicode)
-def test_a_unicode(u):
+@forall(tries=10, u=unicodes)
+def test_unicodes(u):
     assert type(u) == unicode
 
-@forall(tries=10, u=a_unicode(size=(1,1)))
-def test_a_unicode_size(u):
+@forall(tries=10, u=unicodes(size=(1,1)))
+def test_unicodes_size(u):
     assert len(u) == 1
 
 def random_int_unicode_tuple():
-    return lambda: (evaluate(an_integer), evaluate(a_unicode))
+    return lambda: (evaluate(integers), evaluate(unicodes))
 
-@forall(tries=10, l=a_list(item_generator=random_int_unicode_tuple))
+@forall(tries=10, l=lists(items=random_int_unicode_tuple))
 def test_a_tupled_list(l):
     for x in l:
         assert type(x[0]) == int and type(x[1]) == unicode
 
-@forall(tries=10, x=an_integer, y=an_integer)
+@forall(tries=10, x=integers, y=integers)
 def test_addition_associative(x, y):
     assert x + y == y + x
 
-@forall(tries=10, l=a_list)
+@forall(tries=10, l=lists)
 def test_reverse_reverse(l):
     assert list(reversed(list(reversed(l)))) == l
 
-@forall(tries=10, c=a_character)
-def test_a_character(c):
+@forall(tries=10, c=characters)
+def test_characters(c):
     assert len(c) == 1
 
-@forall(tries=10, d=a_dict(item_generator=a_unicode, value_generator=a_list))
-def test_a_dict(d):
-    if d is None:
-        raise Exception('This is bad')
+@forall(tries=10, d=dicts(items=unicodes, values=integers))
+def test_dicts(d):
+    for x, y in d.iteritems():
+        assert type(x) == unicode
+        assert type(y) == int
+
+@forall(tries=10, d=dicts(items=unicodes, values=lists, size=(2, 2)))
+def test_dicts_size(d):
+    assert len(d) == 2
     for x, y in d.iteritems():
         assert type(x) == unicode
         assert type(y) == list
