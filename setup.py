7b2625470f08e033cd0c562129ce8592da08eadf
Dan Bravender <dan.bravender@gmail.com>
2009-07-17 22:42:11 +0900

    Shuffled things around, added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='gp_through_unit_tests',
+      version='0.1',
+      description='',
+      author='Dan Bravender',
+      author_email='dan.bravender@gmail.com',
+      url='http://dan.bravender.us',
+      packages=['qc', 'gpu', 'methodfinder'],
+     )
+
